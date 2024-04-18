902fcea2ed0e25980dc9cd780b5122cb6b4d2097
Luís felipe <160934884+IamLiper@users.noreply.github.com>
2024-04-18 19:53:46 +0000

    oi

---
+++ Exercicios/20. exercicio em dupla.py
@@ -0,0 +1,22 @@
+import os
+os.system("clear")
+
+idade = int
+sexo = str
+salario = float
+escolha = str
+soma = 0
+media = 0
+contador = 0
+
+while True:
+    print("=== Menu ===")
+    escolha = input("1. Adicionar\n2. Exibir resultado\n3. Sair\nR= ")
+    if escolha == '1':
+        idade = input("Digite sua idade: ")
+        sexo = input("Informe o seu sexo (m/f): ")
+        salario = input("nforme o seu sálario: ")
+        contador += 1
+    elif escolha == '2':
+        media =  
+        soma += soma / contador
