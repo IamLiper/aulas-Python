edde9ba1110a50f8073db140a576496bf3530488
Luís felipe <160934884+IamLiper@users.noreply.github.com>
2024-04-19 19:02:26 +0000

    commit

--- Exercicios/24. vetor3.py
+++ Exercicios/24. vetor3.py
@@ -9,13 +9,12 @@ QtdN = 0
 for i in range(10):
     nota = float(input(f"Digite a sua nota {i+1}:"))
     notas.append(nota)
-
     contador += 1
     if nota < 0:
         QtdN += 1
     if nota >= 0:
         soma += nota
-    os.system("clear")
+os.system("clear")
 
 print(f"Soma positiva: {soma}")
 print(f"Quantidade de números negativos: {QtdN}")
