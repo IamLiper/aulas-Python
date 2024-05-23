1096a91d3100a4165089d4a4122c334eb157f0c5
Luís felipe <160934884+IamLiper@users.noreply.github.com>
2024-05-23 18:50:47 +0000

    lil kang é demais vei

--- Exercicios/Ex35,36.py
+++ Exercicios/Ex35,36.py
@@ -2,6 +2,27 @@ import os
 from dataclasses import dataclass
 os.system("clear")
 
+arquivo = "Catálogo_Livros.txt"
+QUANTIDADE_LIVROS = 3
+livros = []
+
+def salvar():
+    for i in range(QUANTIDADE_LIVROS):
+        livro = Livros(
+            nome = input("Digite o nome do livro: "),
+            autor = input("Digite o nome do autor: "),
+            categoria = input("Digite a categoria do livro: "),
+            preco = float(input("Digite o preço do livro: "))
+        )
+        livros.append(livro)
+        os.system("clear")
+
+    with open(arquivo, 'w') as arquivoDeLivros:
+        for livro in livros:
+            arquivoDeLivros.write(f"Nome: {livro.nome}, Autor: {livro.autor}, Categoria: {livro.categoria}, Preço: {livro.preco} \n")
+
+    print("Dados do livro foram salvos!")
+
 def ler_dados_catalogo():
     with open(arquivo, "r") as arquivoDeLivros:
         livrosCad = arquivoDeLivros.read()
@@ -14,26 +35,5 @@ class Livros:
     categoria: str
     preco: float
 
-QUANTIDADE_LIVROS = 3
-
-livros = []
-
-for i in range(QUANTIDADE_LIVROS):
-    livro = Livros(
-        nome = input("Digite o nome do livro: "),
-        autor = input("Digite o nome do autor: "),
-        categoria = input("Digite a categoria do livro: "),
-        preco = float(input("Digite o preço do livro: "))
-    )
-    livros.append(livro)
-    os.system("clear")
-
-arquivo = "Catálogo_Livros.txt"
-
-with open(arquivo, 'w') as arquivoDeLivros:
-    for livro in livros:
-        arquivoDeLivros.write(f"Nome: {livro.nome}, Autor: {livro.autor}, Categoria: {livro.categoria}, Preço: {livro.preco} \n")
-
-print("Dados do livro foram salvos!")
-
+salvar()
 ler_dados_catalogo()
